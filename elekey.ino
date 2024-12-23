2bf7e9e23ab2c731e4424a3818e75e5120e642a0
Kawauso <161272652+Ishikishik@users.noreply.github.com>
2024-12-24 00:34:17 +0900

    Update elekey.ino

--- elekey.ino
+++ elekey.ino
@@ -3,17 +3,29 @@
 // ハードウェアパッケージは MicroCore を使用
 // 設定は底部に書きます
 void setup() {
-  pinMode(PIN5, OUTPUT);         //PIN7 (ATtiny13a 7番ピン)を、出力モード(=OUTPUT)に設定
+  pinMode(PIN5, OUTPUT);         //５番ピンを出力に設定
+  pinMode(PIN1,INPUT_PULLUP);//1番ピンを長点に設定
+
 }
 
-void loop() {                    //2秒ごとに、点滅を10回繰り返し、ループを抜け終了するプログラム
+void loop() {   
+  int longp;
+  longp = digitalRead(PIN1);
+
+  if( longp == LOW ){               //長点の処理  
 digitalWrite(PIN5,HIGH);
 delay(500);
 digitalWrite(PIN5,LOW);
 delay(500);
+}else{digitalWrite(PIN5,LOW);
+}
+
+
 
 
 }
+
+
 
 
 
